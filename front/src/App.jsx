416a223aa3a9ecaa013fa17c756b8880d4ed0425
Fangral <fangri@yandex.ru>
2022-08-23 00:55:08 +0500

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/pages/Home/Home.jsx'
+import Layout from './components/common/Layout.jsx'
+
+ const App = () => {
+  return (
+    <Layout>
+        <Home/>
+    </Layout>
+  )
+}
+export default App;
